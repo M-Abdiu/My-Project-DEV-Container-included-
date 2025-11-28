948e37dd001bef23bcd1653af4b417baf0a01922
M-Abdiu <mehmedali.abdiu@students.fhnw.ch>
2025-11-28 08:55:37 +0100

    Neue CSV aufstellung

--- FileHandling.py
+++ FileHandling.py
@@ -3,9 +3,10 @@ import csv
 def read_csv_input(filename):
     try:
         with open(filename, mode='r', newline='', encoding='utf-8') as file:
-            reader = csv.reader(file)
+            reader = csv.reader(file)       # csv.reader aus dem csv Modul, gibt alles als eine Liste von str wieder.
+            val_arbeitszeiten(reader)
             for row in reader:
-                print(row)   # Print each row placehgolder for now
+                print(row)   # Print each row placeholder for now
                 
     #ExceptionHandling
     except FileNotFoundError:
@@ -13,6 +14,15 @@ def read_csv_input(filename):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+def val_arbeitszeiten(reader):
+    aktuelle_person = None
+    pensum = None
+
+    for row in reader:
+        row.strip() 
+
+    
+
 # Read File path and call function
-csv_file = "data\input.csv"
+csv_file = "data\Berisha, Linda(Alle).csv"
 read_csv_input(csv_file)
